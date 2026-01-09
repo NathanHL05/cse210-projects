43c9d316e29e8ad82743bef12ed85eb3abb37ee9
Nathan <nathanhb.lar@gmail.com>
2026-01-09 13:14:00 -0700

    practice

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -2,8 +2,13 @@ using System;
 
 class Program
 {
+    //just trying new stuff
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Sandbox World!");
+        Console.Write(" And a bit more.");
+        Console.WriteLine("What is your name? ");
+        string user = Console.ReadLine();
+        Console.WriteLine($"your name is: {user}");
     }
 }

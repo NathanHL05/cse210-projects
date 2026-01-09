9bfc47aeb0f39510a6099721b97207ee08b444f2
Nathan <nathanhb.lar@gmail.com>
2026-01-09 13:59:45 -0700

    add grade analysis as instructed

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -3,7 +3,38 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep2 World!");
+    {   
+        String letter = "";
+        String passFail = "";
+        Console.Write("What is your percent in the class? ");
+        String scoreStr = Console.ReadLine();
+        int score = int.Parse(scoreStr);
+        if (score >= 90)
+        {
+            letter = "A";
+            passFail = "Passed";
+        }
+        else if (score >= 80)
+        {
+            letter = "B";
+            passFail = "Passed";
+        }
+        else if (score >= 70)
+        {
+            letter = "C";
+            passFail = "Passed";
+        }
+        else if (score >= 60)
+        {
+            letter = "D";
+            passFail = "Failed";
+        }
+        else
+        {
+            letter = "F";
+            passFail = "Failed";
+        }
+        Console.WriteLine($"Based on your score, you have received and {letter} in your class.");
+        Console.WriteLine($"You {passFail} your class.");
     }
 }

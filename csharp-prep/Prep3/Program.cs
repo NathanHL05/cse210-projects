969a882381770f191799c5ce88831e4ac31c7b4a
Nathan <nathanhb.lar@gmail.com>
2026-01-13 13:33:47 -0700

    number guessing as per project 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        string playAgain = "yes";
+        while (playAgain == "yes")
+        {
+            bool correct = false;
+            Random randomGenerator = new Random();
+            int keyNumber = randomGenerator.Next(0, 50);
+
+            do
+            {
+                Console.WriteLine("Guess a number from 0-50: ");
+                int guess = int.Parse(Console.ReadLine());
+
+                if (guess == keyNumber)
+                {
+                    Console.WriteLine("You guessed it!\n");
+                    correct = true;
+                }
+                else if (guess < keyNumber)
+                {
+                    Console.WriteLine("Higher\n");
+                }
+                else if (guess > keyNumber)
+                {
+                    Console.WriteLine("Lower\n");
+                }
+            } while (correct == false);
+            Console.Write("Do you want to play again? (yes/no)");
+            playAgain = Console.ReadLine();
+        }
     }
 }

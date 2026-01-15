bbf2d1429b44aa97d8164db4671f7a92700e2fcd
Nathan <nathanhb.lar@gmail.com>
2026-01-14 18:15:00 -0700

    added according to project instructions

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,53 @@
 using System;
+using System.Security.AccessControl;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        string name = PromptUserName();
+        int number = PromptUserNumber();
+        int birthYear;
+        PromptUserBirthYear(out birthYear);
+        int square = SquareNumber(number);
+        DisplayResult(name, square, birthYear);
+
+    }
+
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+
+    static string PromptUserName()
+    {
+        Console.Write("What is your name? ");
+        return Console.ReadLine();
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.Write("What is your favorite number? ");
+        return int.Parse(Console.ReadLine());
+    }
+
+    static void PromptUserBirthYear(out int year)
+    {
+        Console.Write("What is your birth year? ");
+        year = int.Parse(Console.ReadLine());
+    }
+
+    static int SquareNumber(int number)
+    {
+        return number*number;
+    }
+
+    static void DisplayResult (string name, int square,int birthYear)
+    {
+        int currentYear = DateTime.Now.Year;
+        int years = currentYear - birthYear;
+        Console.WriteLine($"{name}, the square of your number is {square}.");
+        Console.WriteLine($"{name}, you will turn {years} this year.");
     }
 }

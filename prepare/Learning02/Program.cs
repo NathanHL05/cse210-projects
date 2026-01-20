1639e2d91ff1ecce3c20a94d20707b44a505a457
Nathan <nathanhb.lar@gmail.com>
2026-01-20 16:58:53 -0700

    program.cs, Resume, and Job added so that program works like instructed

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,47 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+   
+        Job job1 = new Job();
+
+        Console.WriteLine("What was your job title: ");
+        job1._jobTitle = Console.ReadLine();
+
+        Console.WriteLine("What company did you work for: ");
+        job1._company = Console.ReadLine();
+
+        Console.WriteLine("What year did you start working there: ");
+        job1._startYear = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("What year did you stop working there: ");
+        job1._endYear = int.Parse(Console.ReadLine());
+
+
+        Job job2 = new Job();
+
+        Console.WriteLine("What was your job title: ");
+        job2._jobTitle = Console.ReadLine();
+
+        Console.WriteLine("What company did you work for: ");
+        job2._company = Console.ReadLine();
+
+        Console.WriteLine("What year did you start working there: ");
+        job2._startYear = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("What year did you stop working there: ");
+        job2._endYear = int.Parse(Console.ReadLine());
+
+
+        Resume myResume = new Resume();
+        Console.WriteLine("What is your name: ");
+        myResume._name = Console.ReadLine();
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+        
+        myResume.Display();
     }
 }

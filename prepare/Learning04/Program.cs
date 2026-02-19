241b68d3833080814d634eeb7ee57ad678a63984
Nathan <nathanhb.lar@gmail.com>
2026-02-19 13:39:41 -0700

    Practice using inheritance, Learning assignment 4

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment assignment = new Assignment("Nathan Larson", "Programming with Classes");
+        Console.WriteLine(assignment.GetSummary());
+        MathAssignment myMath = new MathAssignment("Nathan L", "Multi-Variable Calculus", "4", "1-8");
+        Console.WriteLine(myMath.GetSummary());
+        Console.WriteLine(myMath.GetHomeworkList());
+        WritingAssignment myWriting = new WritingAssignment("John Doe", "English 150", "Super Cool Stuff");
+        Console.WriteLine(myWriting.GetSummary());
+        Console.WriteLine(myWriting.GetWritingInformation());
     }
 }

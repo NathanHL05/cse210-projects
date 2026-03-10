35fe624040a3e5486f3662ddba353c3e28a6a376
Nathan <nathanhb.lar@gmail.com>
2026-03-09 22:49:25 -0600

    added shape class and child classes, used polymorphism

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,22 @@
 using System;
+using System.Formats.Asn1;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        shapes.Add(new Square("red", 2.5));
+        shapes.Add(new Rectangle("blue", 6, 3.5));
+        shapes.Add(new Circle("green", 5));
+
+        foreach (Shape item in shapes)
+        {
+            string color = item.GetColor();
+            double area = item.GetArea();
+            Console.WriteLine($"Color: {color}");
+            Console.WriteLine($"Area: {area}\n");
+        }
     }
 }
